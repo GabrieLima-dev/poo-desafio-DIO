36847e05b776c8733487685d2905c3b371dd870a
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-12-23 16:12:26 -0300

    Project finall

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
-
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
